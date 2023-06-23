fadce3bbbc07890528b13b3996ca158e950d742c
Max Pieshkov <49279392+soulsword1@users.noreply.github.com>
2023-06-23 13:11:50 +0300

    style changes

--- src/components/AppNav/AppNav.styled.js
+++ src/components/AppNav/AppNav.styled.js
@@ -2,9 +2,9 @@ import styled from '@emotion/styled';
 
 export const Header = styled.header`
   display: flex;
-  padding: 0 50px;
+  padding: 10px 50px;
   justify-content: space-between;
   align-items: center;
-  margin-bottom: 16px;
+  margin-bottom: 95px;
   box-shadow: 0px 0px 3px 0px #111;
 `;
